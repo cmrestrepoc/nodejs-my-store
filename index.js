a2199e49101ad5f4d5bf6516202934f533cda08d
CARLOS RESTREPO <carlos.restrepo@bairesdev.com>
2022-09-01 09:05:08 -0500

    Adding 2 basic routes

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello, this is the store in express')
 })
 
+app.get('/new-route', (req, res) => {
+  res.send('Hello, this is a new route')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+})
+
 app.listen(port, () => console.log('listening to port', port))
