9770f7ea34d2ace8fc5151360f8ea062eb8aa5f5
CARLOS RESTREPO <carlos.restrepo@bairesdev.com>
2022-11-06 07:31:37 -0500

    Adding status codes to responses

--- routes/products.router.js
+++ routes/products.router.js
@@ -23,17 +23,23 @@ router.get('/filter', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  const {id} = req.params
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  })
+  const { id } = req.params
+  if (id === '999') { // In url, params are always strings
+    res.status(404).json({
+      message: 'not found'
+    })
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000
+    })
+  }
 })
 
 router.post('/', (req, res) => {
   const body = req.body
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
