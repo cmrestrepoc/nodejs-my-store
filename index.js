ce34063170716f9a7d30b13fbdecf76dcbf685b4
CARLOS RESTREPO <carlos.restrepo@bairesdev.com>
2022-12-14 09:17:40 -0500

    excluding self-origin from whitelist validation

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 const whitelist = ['http://localhost:8080', 'http://myapp.co']
 const options = {
   origin: (origin, callback) => {
-    if(whitelist.includes(origin)) {
+    if(whitelist.includes(origin) || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Origin not allowed'))
