4b0b4175ec408652f5b79fec6246367096b1acbe
CARLOS RESTREPO <carlos.restrepo@bairesdev.com>
2022-12-14 09:07:01 -0500

    Adding cors allowing everybody to access the api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const routerApi = require('./routes')
+const cors = require('cors');
+
 const {
   logErrors,
   errorHandler,
@@ -10,6 +12,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, this is the store in express')
